f59109b506cb4f6a78c254ab0e7e664e38d4e8b3
Marvin <101271135+ms074203@users.noreply.github.com>
2022-08-17 17:21:51 +0800

    2022/08/17 修改首頁展開控制

--- final_report/web/control.js
+++ final_report/web/control.js
@@ -1,16 +1,21 @@
+// 控制首頁 section 展開
+
 const gameStyle = ['#game_style_1', '#game_style_2', '#game_style_3', '#game_style_4', '#game_style_5']
 $(function () {
-    let i = 0
-    gameStyle.forEach(function (step, j) {
+    gameStyle.forEach(function (step, i) {
         $(step).click(function () {
-            i++
-            if (i % 2 == 1) {
+            // 點擊後判斷，已展開的收回
+            if ($(step).css('flex') == '6 1 0%') {
+                $(step).css({
+                    'flex': ''
+                });
+            // 點擊後判斷，未展開的打開，其他展開的收回
+            } else if ($(step).css('flex')) {
+                $('.init_expand').css({
+                    'flex': ''
+                });
                 $(step).css({
                     'flex': '6'
-                });
-            } else {
-                $(step).css({
-                    'flex': '1'
                 });
             }
         });
