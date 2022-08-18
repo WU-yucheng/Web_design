81df769d9636555f1eccd1712c9cbd75860e819a
Marvin <101271135+ms074203@users.noreply.github.com>
2022-08-18 16:43:26 +0800

    2022/08/18 JS首頁按下展開功能修改 CSS顏色變數增加 HTML修改遊戲類型名稱及選擇器名字

--- final_report/web/control.js
+++ final_report/web/control.js
@@ -1,21 +1,29 @@
 // 控制首頁 section 展開
-
-const gameStyle = ['#game_style_1', '#game_style_2', '#game_style_3', '#game_style_4', '#game_style_5']
+const gamestyle_list = ['#game_style_1', '#game_style_2', '#game_style_3', '#game_style_4', '#game_style_5']
 $(function () {
-    gameStyle.forEach(function (step, i) {
+    gamestyle_list.forEach(function (step, i) {
+        // section 裡的東西先全部隱藏起來
+        $(step + ' *').hide();
         $(step).click(function () {
-            // 點擊後判斷，已展開的收回
+            // 點擊後判斷，已展開的收回並隱藏內容
             if ($(step).css('flex') == '6 1 0%') {
                 $(step).css({
                     'flex': ''
                 });
-            // 點擊後判斷，未展開的打開，其他展開的收回
+                $(step + ' *').hide();
+            // 點擊後判斷，未展開的打開，其他展開的收回並隱藏內容
             } else if ($(step).css('flex')) {
                 $('.init_expand').css({
                     'flex': ''
                 });
+                $('.init_expand *').hide();
                 $(step).css({
                     'flex': '6'
+                });
+                $(step + '>*').show()
+                $(step + ' h1').css({
+                    'color': 'var(--color_font)',
+                    'margin-top': '20px',
                 });
             }
         });
